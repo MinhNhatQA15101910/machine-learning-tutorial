5556f136f67bf84b1fab9f5fa48b1e0bd9ce62c4
Do Nhat <dmnhat.pt@gmail.com>
2024-08-08 13:38:20 +0700

    Data preprocessing process.

--- 1. Data Preprocessing/data_preprocessing_tools.py
+++ 1. Data Preprocessing/data_preprocessing_tools.py
@@ -1,4 +1,5 @@
 # Data Preprocessing Tools
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +11,6 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 print(X)
-
 print(y)
 
 # Taking care of missing data
@@ -23,6 +23,7 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
 
 # Encoding categorical data
+
 # Encoding the Independent Variable
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -48,11 +49,8 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 print(X_train)
-
 print(X_test)
-
 print(y_train)
-
 print(y_test)
 
 # Feature Scaling
@@ -63,5 +61,4 @@ X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
 X_test[:, 3:] = sc.transform(X_test[:, 3:])
 
 print(X_train)
-
 print(X_test)
