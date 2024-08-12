2981b1fa6da53f1f0d5234f80fd06ab8514bc208
Do Nhat <dmnhat.pt@gmail.com>
2024-08-12 16:53:58 +0700

    Visualizing the Decision Tree Regression results.

--- 2. Regression/2.4. Support Vector Regression (SVR)/support_vector_regression.py
+++ 2. Regression/2.4. Support Vector Regression (SVR)/support_vector_regression.py
@@ -42,7 +42,7 @@ plt.ylabel("Salary")
 plt.show()
 
 # Visualizing the SVR results (for higher resolution and smoother curve)
-X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.01)
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color="red")
 plt.plot(
