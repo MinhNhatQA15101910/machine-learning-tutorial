50666623f12aaaac4cf29b8a5919c60f088cc22c
Do Nhat <dmnhat.pt@gmail.com>
2024-08-12 16:43:29 +0700

    Training the Decision Tree Regression model on the whole dataset.

--- 2. Regression/2.5. Decision Tree Regression/decision_tree_regression.py
+++ 2. Regression/2.5. Decision Tree Regression/decision_tree_regression.py
@@ -9,3 +9,9 @@ import pandas as pd
 dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+# Training the Decision Tree Regression model on the whole dataset
+from sklearn.tree import DecisionTreeRegressor
+
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
