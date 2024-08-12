77031af20bad0eef610c1a9c899b9e813c15c068
Do Nhat <dmnhat.pt@gmail.com>
2024-08-12 15:53:39 +0700

    Visualizing the SVR results.

--- 2. Regression/2.4. Support Vector Regression (SVR)/support_vector_regression.py
+++ 2. Regression/2.4. Support Vector Regression (SVR)/support_vector_regression.py
@@ -28,3 +28,29 @@ regressor.fit(X, y)
 
 # Predicting a new result
 sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1, 1))
+
+# Visualizing the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color="red")
+plt.plot(
+    sc_X.inverse_transform(X),
+    sc_y.inverse_transform(regressor.predict(X).reshape(-1, 1)),
+    color="blue",
+)
+plt.title("Truth or Bluff (SVR)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
+
+# Visualizing the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color="red")
+plt.plot(
+    X_grid,
+    sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid)).reshape(-1, 1)),
+    color="blue",
+)
+plt.title("Truth or Bluff (SVR)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
