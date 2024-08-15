c183b511023d84b95979dbbd1b09d39726e750f7
Do Nhat <dmnhat.pt@gmail.com>
2024-08-15 11:22:37 +0700

    Training the Logistic Regression model on the Training set.

--- 3. Classification/3.1. Logistic Regression/logistic_regression.py
+++ 3. Classification/3.1. Logistic Regression/logistic_regression.py
@@ -26,3 +26,9 @@ X_test = sc.transform(X_test)
 
 print(X_train)
 print(X_test)
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
