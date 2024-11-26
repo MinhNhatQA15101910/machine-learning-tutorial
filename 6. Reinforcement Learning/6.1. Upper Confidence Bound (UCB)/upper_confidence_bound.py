34f3fb8747432f73dbec4af00bf70ee77a37db21
Do Nhat <dmnhat.pt@gmail.com>
2024-11-26 15:00:50 +0700

    Define variables.

--- 6. Reinforcement Learning/6.1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 6. Reinforcement Learning/6.1. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -7,3 +7,11 @@ import pandas as pd
 
 ## Importing the dataset
 dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
+
+## Implementing UCB
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
