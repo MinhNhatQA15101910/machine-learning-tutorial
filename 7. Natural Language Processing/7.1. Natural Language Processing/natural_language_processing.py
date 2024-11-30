120ca38d5fc3b9535334403a29e1553d9b438bd2
Do Nhat <dmnhat.pt@gmail.com>
2024-11-30 17:17:43 +0700

    Text cleaning for NLP.

--- 7. Natural Language Processing/7.1. Natural Language Processing/natural_language_processing.py
+++ 7. Natural Language Processing/7.1. Natural Language Processing/natural_language_processing.py
@@ -9,6 +9,19 @@ import pandas as pd
 dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter="\t", quoting=3)
 
 ## Cleaning the texts
+import re
+import nltk
+
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+for i in range(0, 1000):
+    review = re.sub("[^a-zA-Z]", " ", dataset["Review"][i])
+    review = review.lower()
+    review = review.split()
+
 ## Creating the Bag of Words model
 ## Splitting the dataset into the Training set and Test set
 ## Training the Naive Bayes model on the Training set
