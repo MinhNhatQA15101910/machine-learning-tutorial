e49fe790baa94dfc17aba65add85ce0edbc91b89
Do Nhat <dmnhat.pt@gmail.com>
2024-12-02 16:22:47 +0700

    Data preprocessing.

--- 8. Deep Learning/8.1. Artificial Neural Networks (ANN)/artificial_neural_network.py
+++ 8. Deep Learning/8.1. Artificial Neural Networks (ANN)/artificial_neural_network.py
@@ -1,10 +1,48 @@
 # Artificial Neural Network
+
 ## Importing the libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+tf.__version__
+
 ## Part 1 - Data Preprocessing
 ### Importing the dataset
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
 ### Encoding categorical data
+#### Label Encoding the "Gender" column
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+#### One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(
+    transformers=[("encoder", OneHotEncoder(), [1])], remainder="passthrough"
+)
+X = np.array(ct.fit_transform(X))
+
+print(X)
+
 ### Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
 ### Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
 ## Part 2 - Building the ANN
 ### Initializing the ANN
 ### Adding the input layer and the first hidden layer
