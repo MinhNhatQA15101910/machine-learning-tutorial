3e84828a73c7800e5775c6ea0e00d7331ceaf1e1
Do Nhat <dmnhat.pt@gmail.com>
2024-12-03 16:39:25 +0700

    Data preprocessing.

--- 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
+++ 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
@@ -1,9 +1,26 @@
 # Convolutional Neural Network
 
 ## Importing the libraries
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+print(tf.__version__)
+
 ## Part 1 - Data Preprocessing
 ### Preprocessing the Training set
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
+)
+training_set = train_datagen.flow_from_directory(
+    "dataset/training_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
+
 ### Preprocessing the Test set
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+test_set = test_datagen.flow_from_directory(
+    "dataset/test_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+)
+
 ## Part 2 - Building the CNN
 ### Initializing the CNN
 ### Step 1 - Convolution
