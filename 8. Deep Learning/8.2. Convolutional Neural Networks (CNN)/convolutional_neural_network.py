c5773c86d5e27ba7d97017537b5ca07faf296482
Do Nhat <dmnhat.pt@gmail.com>
2024-12-03 15:41:09 +0700

    Model outline.

---
+++ 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
@@ -0,0 +1,18 @@
+# Convolutional Neural Network
+
+## Importing the libraries
+## Part 1 - Data Preprocessing
+### Preprocessing the Training set
+### Preprocessing the Test set
+## Part 2 - Building the CNN
+### Initializing the CNN
+### Step 1 - Convolution
+### Step 2 - Pooling
+### Adding a second convolutional layer
+### Step 3 - Flattening
+### Step 4 - Full Connection
+### Step 5 - Output layer
+## Part 3 - Training the CNN
+### Compiling the CNN
+### Training the CNN on the Training set and evaluating it on the Test set
+## Part 4 - Making a single prediction
