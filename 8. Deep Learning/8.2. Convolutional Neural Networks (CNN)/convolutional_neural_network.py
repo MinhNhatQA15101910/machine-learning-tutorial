345283ba7522b272fc66bb1149f56d8c4cdd1a9b
Do Nhat <dmnhat.pt@gmail.com>
2024-12-09 22:45:09 +0700

    Building the CNN.

--- 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
+++ 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
@@ -23,12 +23,31 @@ test_set = test_datagen.flow_from_directory(
 
 ## Part 2 - Building the CNN
 ### Initializing the CNN
+cnn = tf.keras.models.Sequential()
+
 ### Step 1 - Convolution
+cnn.add(
+    tf.keras.layers.Conv2D(
+        filters=32, kernel_size=3, activation="relu", input_shape=[64, 64, 3]
+    )
+)
+
 ### Step 2 - Pooling
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
 ### Adding a second convolutional layer
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation="relu"))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
 ### Step 3 - Flattening
+cnn.add(tf.keras.layers.Flatten())
+
 ### Step 4 - Full Connection
+cnn.add(tf.keras.layers.Dense(units=128, activation="relu"))
+
 ### Step 5 - Output layer
+cnn.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
+
 ## Part 3 - Training the CNN
 ### Compiling the CNN
 ### Training the CNN on the Training set and evaluating it on the Test set
