036fdbbec2f51b9b40c4557f956824b487fd7e28
Do Nhat <dmnhat.pt@gmail.com>
2024-12-09 23:15:26 +0700

    Training the CNN and making a single prediction.

--- 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
+++ 8. Deep Learning/8.2. Convolutional Neural Networks (CNN)/convolutional_neural_network.py
@@ -50,5 +50,25 @@ cnn.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
 
 ## Part 3 - Training the CNN
 ### Compiling the CNN
+cnn.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
 ### Training the CNN on the Training set and evaluating it on the Test set
+cnn.fit(x=training_set, validation_data=test_set, epochs=25)
+
 ## Part 4 - Making a single prediction
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+test_image = image.load_img(
+    "dataset/single_prediction/cat_or_dog_1.jpg", target_size=(64, 64)
+)
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = "dog"
+else:
+    prediction = "cat"
+
+print(prediction)
